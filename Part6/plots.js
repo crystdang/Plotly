f117822c24215e8b5f90e650e4579e7c0c95ae59
crystdang <111620460+crystdang@users.noreply.github.com>
2022-11-24 00:51:35 -0500

    Dev 1 incomplete

--- Part6/plots.js
+++ Part6/plots.js
@@ -28,9 +28,18 @@ function init() {
       var PANEL = d3.select("#sample-metadata");
   
       PANEL.html("");
-      PANEL.append("h6").text(result.location);
+      // PANEL.append("h6").text(["ID: " + result.id]);
+      // PANEL.append("h6").text(["ETHNICITY: " + result.ethnicity]);
+      // PANEL.append("h6").text(["GENDER: " + result.gender]);
+      // PANEL.append("h6").text(["AGE: " + result.age]);
+      // PANEL.append("h6").text(["LOCATION: " + result.location]);
+      // PANEL.append("h6").text(["BBTYPE: " + result.bbtype]);
+      // PANEL.append("h6").text(["WFREQ: " + result.wfreq]);
+      Object.entries(result).forEach(([key, value]) => {
+        PANEL.append("h6").text(`${key.toUpperCase()}: ${value}`);
+      });
     });
   }
-
   // SKILL DRILL Open VS Code and modify the buildMetadata() function to populate the Demographic Info panel 
   // with the rest of the demographic data when a menu option is selected:
+  // ID, ETHNICITY, GENDER, AGE, LOCATION, BBTYPE, WFREQ
